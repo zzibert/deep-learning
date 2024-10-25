c4c74454237696223e36872a172e3847b59de296
Žan Žibert <zzibert@plume.com>
2024-10-25 10:21:48 +0200

    3.4.7 The Need for Speed: vector vs for loop

--- vectorization.py
+++ vectorization.py
@@ -1,7 +1,44 @@
 import numpy as np    # it is an unofficial standard to use np for numpy
 import time
 
-# NumPy routines which allocate memory and fill arrays with value
-a = np.zeros(4);                print(f"np.zeros(4) :   a = {a}, a shape = {a.shape}, a data type = {a.dtype}")
-a = np.zeros((4,));             print(f"np.zeros(4,) :  a = {a}, a shape = {a.shape}, a data type = {a.dtype}")
-a = np.random.random_sample(4); print(f"np.random.random_sample(4): a = {a}, a shape = {a.shape}, a data type = {a.dtype}")
+a = np.array([ 1, 2, 3, 4])
+b = np.array([-1,-2, 3, 4])
+print(f"Binary operators work element wise: {a + b}")
+
+a = np.array([1, 2, 3, 4])
+
+def my_dot(a, b): 
+    """
+   Compute the dot product of two vectors
+ 
+    Args:
+      a (ndarray (n,)):  input vector 
+      b (ndarray (n,)):  input vector with same dimension as a
+    
+    Returns:
+      x (scalar): 
+    """
+    x=0
+    for i in range(a.shape[0]):
+        x = x + a[i] * b[i]
+    return x
+
+np.random.seed(1)
+a = np.random.rand(10000000)  # very large arrays
+b = np.random.rand(10000000)
+
+tic = time.time()  # capture start time
+c = np.dot(a, b)
+toc = time.time()  # capture end time
+
+print(f"np.dot(a, b) =  {c:.4f}")
+print(f"Vectorized version duration: {1000*(toc-tic):.4f} ms ")
+
+tic = time.time()  # capture start time
+c = my_dot(a,b)
+toc = time.time()  # capture end time
+
+print(f"my_dot(a, b) =  {c:.4f}")
+print(f"loop version duration: {1000*(toc-tic):.4f} ms ")
+
+del(a);del(b)  #remove these big arrays from memory
