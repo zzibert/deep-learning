991765db65b1fbef602c84e79681c77cd750b6da
Žan Žibert <zzibert@plume.com>
2024-10-25 08:30:01 +0200

    vector creation0

---
+++ vectorization.py
@@ -0,0 +1,7 @@
+import numpy as np    # it is an unofficial standard to use np for numpy
+import time
+
+# NumPy routines which allocate memory and fill arrays with value
+a = np.zeros(4);                print(f"np.zeros(4) :   a = {a}, a shape = {a.shape}, a data type = {a.dtype}")
+a = np.zeros((4,));             print(f"np.zeros(4,) :  a = {a}, a shape = {a.shape}, a data type = {a.dtype}")
+a = np.random.random_sample(4); print(f"np.random.random_sample(4): a = {a}, a shape = {a.shape}, a data type = {a.dtype}")
